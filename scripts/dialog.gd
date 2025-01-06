29f683b4d2576964b53721b593a1dcafa3033972
Hamza Sehavdic <hsehavdic@gmail.com>
2025-01-07 00:29:31 +1100

    Restarting project. Removing unnecessary files for simplest game

--- scripts/dialog.gd
+++
@@ -1,8 +0,0 @@
-class_name Dialog
-extends Node2D
-
-# Make Array where each elem is a Dict called DialogEntry
-# DialogEntry key is a String to annotate Labels in a TextBox/TextureRect
-# DialogEntry value is a AudioStream file (pref .wav) to be played during label is being rendered
-@export var dialog_lines: Array[DialogEntry] = [DialogEntry.new()]
-
--- scripts/dialog_entry.gd
+++
@@ -1,5 +0,0 @@
-class_name DialogEntry
-extends Resource
-
-@export var dialog_text: String
-@export var dialog_WAV_file: AudioStream
